4d48d16ec484d448feebd0a80b73a32dc6d64948
quentinzhang <tagriver@gmail.com>
2023-06-21 18:39:50 +0800

    add function calls

--- src/components/Chat/ChatContent/ScrollToBottomButton.tsx
+++ src/components/Chat/ChatContent/ScrollToBottomButton.tsx
@@ -7,12 +7,17 @@ const ScrollToBottomButton = React.memo(() => {
   const scrollToBottom = useScrollToBottom();
   const [atBottom] = useAtBottom();
 
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    scrollToBottom();
+  };
+
   return (
     <button
       className={`cursor-pointer absolute right-6 bottom-[80px] md:bottom-[80px] z-10 rounded-full border border-gray-200 bg-gray-50 text-gray-600 dark:border-white/10 dark:bg-white/10 dark:text-gray-200 ${
         atBottom ? 'hidden' : ''
       }`}
-      onClick={scrollToBottom}
+      onClick={handleClick}
     >
       <DownArrow />
     </button>
